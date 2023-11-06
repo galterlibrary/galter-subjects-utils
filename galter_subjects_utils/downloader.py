548e08a5a61674c4c2135d2a72c515176fe742f7
Guillaume Viger <fenekku@fenekku.com>
2023-11-06 10:47:13 -0500

    add LCSH generation for InvenioRDM

--- galter_subjects_utils/downloader.py
+++ galter_subjects_utils/downloader.py
@@ -8,9 +8,19 @@
 
 """Download MeSH file."""
 
+import gzip
 import shutil
 
 import requests
+
+
+def download_file(url, dest):
+    """Download a file."""
+    with requests.get(url, stream=True) as req:
+        with open(dest, 'wb') as f:
+            shutil.copyfileobj(req.raw, f)
+
+    return dest
 
 
 class MeSHDownloader:
@@ -36,15 +46,38 @@ class MeSHDownloader:
         for prefix in self.prefix_to_filepath.keys():
             if self.cache and self.prefix_to_filepath[prefix].exists():
                 continue
-            self.download_file(
+            download_file(
                 self.base_url + f"{prefix}{self.year}.bin",
                 self.prefix_to_filepath[prefix],
             )
 
-    def download_file(self, url, dest):
-        """Download a file."""
-        with requests.get(url, stream=True) as req:
-            with open(dest, 'wb') as f:
-                shutil.copyfileobj(req.raw, f)
 
-        return dest
+def extract_file(in_filepath, out_filepath):
+    """Extract gzipped file."""
+    with gzip.open(in_filepath, 'rb') as f_in:
+        with open(out_filepath, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+    return out_filepath
+
+
+class LCSHDownloader:
+    """Download LCSH file."""
+
+    def __init__(self, directory, cache=False):
+        """Constructor."""
+        self.base_url = "https://id.loc.gov/download/authorities/subjects.skosrdf.jsonld.gz"  # noqa
+        self.directory = directory
+        self.filepath = self.directory / self.base_url.rsplit("/")[-1]
+        self.cache = cache
+
+    def download(self):
+        """Download LCSH files of interest."""
+        if not self.cache or not self.filepath.exists():
+            download_file(self.base_url, self.filepath)
+            # sneak extraction in caching: bit of a cheat, but ok for now
+            extract_file(self.filepath, self.filepath.with_suffix(""))
+
+        self.downloaded_filepath = self.filepath  # just a marker + future
+        self.extracted_filepath = self.filepath.with_suffix("")
+        return self.extracted_filepath
