d929ef07939f98b3e3a668e6646140ef8ddea3e1
Guillaume Viger <fenekku@fenekku.com>
2024-04-02 16:31:21 -0400

    [#682] generate deprecated file with replaced id/subject if can
    closes #682

--- galter_subjects_utils/contrib/lcsh/converter.py
+++ galter_subjects_utils/contrib/lcsh/converter.py
@@ -8,6 +8,9 @@
 
 """Generic conversion functionality."""
 
+import re
+from datetime import datetime
+
 from .scheme import LCSHScheme
 
 
@@ -106,6 +109,23 @@ def is_deprecated(topic):
     return bool(find_deprecation_node(topic, node_main))
 
 
+def happened_since(topic, since=None):
+    """Filter for topic having happened after since.
+
+    :param topic: skos topic
+    :type since: dict
+    :param since: date since to filter by (included)
+    :type since: datetime.datetime
+    """
+    if not since:
+        return True
+    node_main = find_main_node(topic)
+    node_deprecation = find_deprecation_node(topic, node_main)
+    date_created_str = node_deprecation.get("cs:createdDate", {}).get("@value")
+    date_created = datetime.strptime(date_created_str, "%Y-%m-%dT%H:%M:%S")
+    return since <= date_created
+
+
 def find_explanation(nodes_of_change_note):
     """Find explaination among skos:changeNote nodes."""
     for n in nodes_of_change_note:
@@ -115,25 +135,47 @@ def find_explanation(nodes_of_change_note):
     return ""
 
 
+pattern = r"covered by .+ subject heading( {(?P<subject_new>.+)})? .+(?P<id_new>sh\s?\d+)$"  # noqa
+regex_for_replacement = re.compile(pattern)
+
+
+def extract_replacement_id_subject(notes):
+    """Returns (id_new, subject_new) tuple from notes or ("", "") else."""
+    matches = regex_for_replacement.search(notes)
+    if not matches:
+        return "", ""
+    id_new = matches.group("id_new") or ""
+    id_new = id_new.replace(" ", "")  # get rid of potential space after 'sh'
+    lcsh = LCSHScheme()
+    id_new = lcsh.generate_id(id_new) if id_new else id_new
+    subject_new = matches.group("subject_new") or ""
+    return id_new, subject_new
+
+
 def to_raw_replacement(topic):
     """Fill out replacement dict."""
     node_main = find_main_node(topic)
     node_deprecation = find_deprecation_node(topic, node_main)
+    notes = find_explanation(node_main.get("skos:changeNote"))
+    id_new, subject_new = extract_replacement_id_subject(notes)
     return {
         "id": node_main.get("@id").replace("http://", "https://"),
         "time": node_deprecation.get("cs:createdDate", {}).get("@value"),
         "subject": node_main.get("skosxl:literalForm", {}).get("@value"),
-        "new_id": "TODO:Expert:Fill me",
-        "new_subject": "TODO:Expert:Fill me",
-        "notes": find_explanation(node_main.get("skos:changeNote")),
+        "new_id": id_new,
+        "new_subject": subject_new,
+        "notes": notes,
     }
 
 
-def deprecated_to_replacements(topics):
+def raw_to_deprecated(topics, since=None):
     """Iterator over deprecated topics and their relevant information.
 
     :param topics: LCSH Topics iterable
     :type topics: iterable[dict]
+    :param since: filter for this date (included) and later
+    :type topics: datetime.datetime
+
     :yields: Replacement dicts of the shape:
         {
             "id": ...,
@@ -147,6 +189,10 @@ def deprecated_to_replacements(topics):
     for topic in topics:
         # filter for deprecated
         if not is_deprecated(topic):
+            continue
+
+        # filter for date
+        if not (happened_since(topic, since)):
             continue
 
         # fill out result
--- tests/contrib/lcsh/test_converter.py
+++ tests/contrib/lcsh/test_converter.py
@@ -6,10 +6,11 @@
 # modify it under the terms of the MIT License; see LICENSE file for more
 # details.
 
+from datetime import datetime
 from pathlib import Path
 
 from galter_subjects_utils.contrib.lcsh.converter import LCSHRDMConverter, \
-    deprecated_to_replacements
+    raw_to_deprecated
 from galter_subjects_utils.reader import read_jsonl
 
 
@@ -48,16 +49,65 @@ def test_deprecated():
     filepath = Path(__file__).parent / "data" / "fake_lcsh.skosrdf.jsonld"
     topics = list(read_jsonl(filepath))
 
-    dicts_of_replacements = [d for d in deprecated_to_replacements(topics)]
+    dicts_of_replacements = [d for d in raw_to_deprecated(topics)]
 
     expected = [
+        # No clear new_id/new_subject
         {
             "id": "https://id.loc.gov/authorities/subjects/sh00000273",
             "time": "2021-07-20T08:25:20",
             "subject": "Child concentration camp inmates",
-            "new_id": "TODO:Expert:Fill me",
-            "new_subject": "TODO:Expert:Fill me",
+            "new_id": "",
+            "new_subject": "",
             "notes": "This authority record has been deleted because the heading is covered by the subject headings {Child internment camp inmates} (DLC)sh2021004026 and {Child Nazi concentration camp inmates} (DLC)sh2021004027"  # noqa
-        }
+        },
+        # Clear new_id/new_subject
+        {
+            "id": "https://id.loc.gov/authorities/subjects/sh2008007279",
+            "time": "2023-01-23T12:01:15",
+            "subject": "Computer games industry",
+            "new_id": "https://id.loc.gov/authorities/subjects/sh2006005259",
+            "new_subject": "Video games industry",
+            "notes": "This authority record has been deleted because the heading is covered by the subject heading {Video games industry} (DLC)sh2006005259"  # noqa
+        },
+        # Variation: strange formatting to account for
+        {
+            "id": "https://id.loc.gov/authorities/subjects/sh2006004185",
+            "time": "2023-01-23T12:01:15",
+            "subject": "Computer games--Law and legislation",
+            "new_id": "https://id.loc.gov/authorities/subjects/sh85143203",
+            "new_subject": "Video games--Law and legislation",
+            "notes": "This authority record has been deleted because the heading is covered by the subject heading {Video games--Law and legislation} (DLC)sh 85143203"  # noqa
+        },
+        # Variation: different wording + no replacement subject
+        {
+            "id": "https://id.loc.gov/authorities/subjects/sh2022001344",
+            "time": "2023-09-29T18:42:32",
+            "subject": "Evangelisch-Lutherse Kerk (Netherlands)--Relations--Nederlandse Hervormde Kerk",  # noqa
+            "new_id": "https://id.loc.gov/authorities/subjects/sh2022001319",
+            "new_subject": "",
+            "notes": "This authority record has been deleted because the heading is covered by an identical subject heading (DLC)sh2022001319"  # noqa
+        },
+    ]
+    assert expected == dicts_of_replacements
+
+
+def test_deprecated_since():
+    filepath = Path(__file__).parent / "data" / "fake_lcsh.skosrdf.jsonld"
+    topics = list(read_jsonl(filepath))
+
+    dicts_of_replacements = [
+        d for d in raw_to_deprecated(topics, datetime(2023, 9, 29))
+    ]
+
+    expected = [
+        {
+            "id": "https://id.loc.gov/authorities/subjects/sh2022001344",
+            "time": "2023-09-29T18:42:32",
+            "subject": "Evangelisch-Lutherse Kerk (Netherlands)--Relations--Nederlandse Hervormde Kerk",  # noqa
+            "new_id": "https://id.loc.gov/authorities/subjects/sh2022001319",
+            "new_subject": "",
+            "notes": "This authority record has been deleted because the heading is covered by an identical subject heading (DLC)sh2022001319"  # noqa
+        },
     ]
     assert expected == dicts_of_replacements
